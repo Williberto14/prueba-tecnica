43963a25deedb944030351ab115aa2fd5d4ee3c3
Williberto <williberto14@gmail.com>
2021-11-16 01:06:26 -0500

    agregado endpoint volumes autenticacion funcionando

--- conf/conf-db-gorm.go
+++ conf/conf-db-gorm.go
@@ -3,23 +3,22 @@ package conf
 import (
 	"fmt"
 
-	"github.com/Williberto14/prueba-tecnica/entity"
+	"github.com/Williberto14/prueba-tecnica/model"
 	"github.com/jinzhu/gorm"
 )
-
 
 var db *gorm.DB
 var err error
 
-func ConfDatabase() *gorm.DB{
-	
+func ConfDatabase() *gorm.DB {
+
 	db, err = gorm.Open("sqlite3", "./database/products.db")
-	
+
 	if err != nil {
 		fmt.Println(err)
-	 }
+	}
 
-	 db.AutoMigrate(&entity.Product{})
+	db.AutoMigrate(&model.Product{})
 
 	return db
 }
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/Williberto14/prueba-tecnica/conf"
-	"github.com/Williberto14/prueba-tecnica/entity"
+	"github.com/Williberto14/prueba-tecnica/model"
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -12,24 +15,34 @@ import (
 var db = conf.ConfDatabase()
 
 func main() {
-
 	defer db.Close()
 
 	r := gin.Default()
-	r.GET("/products/", GetProducts)
-	r.GET("/product/:id", GetProduct)
-	r.POST("/products", CreateProduct)
-	r.PUT("/products/:id", UpdateProduct)
-	r.DELETE("/products/:id", DeleteProduct)
+
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"user1": "root",
+		"user2": "1234",
+	}))
+
+	authorized.GET("/volumes", GetVolumes)
+
+	r.POST("/products/create", CreateProduct)
+	r.GET("/products/list", GetProducts)
+	r.GET("/products/find/:id", GetProduct)
+	r.PUT("/products/update/:id", UpdateProduct)
+	r.DELETE("/products/delete/:id", DeleteProduct)
+
 	r.Run(":9098")
 }
 
+//=====================================================================================
+
 func CreateProduct(c *gin.Context) {
-	var product entity.Product
+	var product model.Product
 	c.BindJSON(&product)
 	id := product.Product_id
 	if found := db.Where("product_id = ?", id).First(&product).Error; found == nil {
-		c.JSON(400, "Error! ya existe un producto con el ID: "+id)
+		c.JSON(400, "Error! ya existe un producto con id: "+id)
 	} else {
 		db.Create(&product)
 		c.JSON(200, product)
@@ -38,9 +51,9 @@ func CreateProduct(c *gin.Context) {
 
 func GetProduct(c *gin.Context) {
 	id := c.Params.ByName("id")
-	var product entity.Product
+	var product model.Product
 	if err := db.Where("product_id = ?", id).First(&product).Error; err != nil {
-		c.JSON(404, "No existe un producto con el ID: "+id)
+		c.JSON(404, "No existe un producto con id: "+id)
 		fmt.Println(err)
 	} else {
 		c.JSON(200, product)
@@ -48,7 +61,7 @@ func GetProduct(c *gin.Context) {
 }
 
 func GetProducts(c *gin.Context) {
-	var products []entity.Product
+	var products []model.Product
 	if err := db.Find(&products).Error; err != nil {
 		c.AbortWithStatus(404)
 		fmt.Println(err)
@@ -58,27 +71,34 @@ func GetProducts(c *gin.Context) {
 }
 
 func UpdateProduct(c *gin.Context) {
-	var product entity.Product
+	var product model.Product
 	id := c.Params.ByName("id")
 	if err := db.Where("product_id  = ?", id).First(&product).Error; err != nil {
-		c.JSON(404, "Error! No se pudo actualizar el producto con ID: "+id)
+		c.JSON(404, "Error! No se pudo actualizar el producto con id: "+id)
 		fmt.Println(err)
 	} else {
 		c.BindJSON(&product)
 		db.Save(&product)
 		c.JSON(200, product)
 	}
-
 }
 
 func DeleteProduct(c *gin.Context) {
 	id := c.Params.ByName("id")
-	var product entity.Product
+	var product model.Product
 	d := db.Where("product_id  = ?", id)
 	if err := db.Where("product_id  = ?", id).First(&product).Error; err != nil {
-		c.JSON(404, "No existe un producto con ID: "+id)
+		c.JSON(404, "No existe un producto con id: "+id)
 	} else {
 		d.Delete(&product)
-		c.JSON(200, gin.H{"id #" + id: "deleted"})
+		c.JSON(200, gin.H{"Producto con id #" + id: "eliminado con exito"})
 	}
+}
+
+func GetVolumes(c *gin.Context) {
+	fmt.Println("Estamos vivos")
+	jsonFile, _ := ioutil.ReadFile("./volumes/volumen_list.json")
+	var data interface{}
+	json.Unmarshal(jsonFile, &data)
+	c.JSON(http.StatusOK, data)
 }
